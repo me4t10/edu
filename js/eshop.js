21bbfa8ebd4fe6e2c1ddbae665f28e8dc565419d
Kukushkin Maksim <me4t10@gmail.com>
2020-06-26 13:52:47 +0700

    Add files via upload

--- js/eshop.js
+++ js/eshop.js
@@ -1,3 +1,5 @@
+var cart = {}; //моя корзина
+
 $('document').ready(function(){
     loadGoods();
 });
@@ -5,15 +7,32 @@ $('document').ready(function(){
 function loadGoods() {
     //загружаю товары на страницу
     $.getJSON('goods.json', function (data) {
+        //console.log(data);
         var out = '';
         for (var key in data){
             out+='<div class="single-goods">';
             out+='<h3>'+data[key]['name']+'</h3>';
             out+='<p>Цена: '+data[key]['cost']+'</p>';
             out+='<img src="'+data[key].image+'">';
-            out+='<button>Купить</button>';
+            out+='<button class="add-to-cart" data-art="'+key+'">Купить</button>';
             out+='</div>';
         }
         $('#goods').html(out);
-    })
+        $('button.add-to-cart').on('click', addToCart);
+    });
+}
+
+function addToCart() {
+    //добавляем товар в корзину
+    var articul = $(this).attr('data-art');
+    if (cart[articul]!=undefined) {
+        cart[articul]++;
+    }
+    else {
+        cart[articul] = 1;
+    }
+
+    console.log(cart);
+
+
 }
